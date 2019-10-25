a667de27bed5b7163d7bbb8eb6340381bf109295
Ivan <ivanchernov056@gmail.com>
2019-10-25 21:26:23 +0700

    add main, CMake and types

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include    "types.h"
+#include    <iostream>
+
+
+
+int main (int argc, char* argv[]) {
+
+    Matrix_t    A;
+    std::cout << A << "\n";
+    
+    return 0;
+}
---
+++ types.h
@@ -0,0 +1,14 @@
+#ifndef     TYPES_H
+#define     TYPES_H
+
+
+#include    <armadillo>
+using   Matrix_t = arma::mat;
+using   Column_t = arma::vec;
+using   Row_t    = arma::rowvec;
+using   SpMat_t  = arma::sp_mat;
+
+#include    <vector>
+
+
+#endif
